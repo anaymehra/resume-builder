4ab1df2aa845073ea3e9e3b4487a5e6bb6541660
Anay Mehra <anaymehra63@gmail.com>
2024-10-15 00:31:21 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -296,7 +296,7 @@ app.post('/submit', authenticateToken, async (req, res) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
