92286f982d5487baa775b4c1636a6e38e6f2ed91
Anay Mehra <anaymehra63@gmail.com>
2024-10-15 02:56:41 +0530

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -20,17 +20,9 @@ const app = express();
 const allowedOrigins = [
     "https://resume-builder-nu-gray.vercel.app"
   ];
-  
-  app.use(cors({
-    origin: function(origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ["POST"],
     credentials: true,
   }));
 
