75615943672bb7731ab4d7cebdcb5bfa15071a35
Anay Mehra <anaymehra63@gmail.com>
2025-03-10 21:22:05 +0530

    fix: deployement

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid'
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid' 
 
 const Auth = () => {
   const location = useLocation()
